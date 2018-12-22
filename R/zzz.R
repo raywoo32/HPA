c7606462b6cd06a43365e54228666bd35356c04a
hyginn <boris.steipe@utoronto.ca>
2018-12-22 16:59:47 +1000

    Startup message works, but works only once per session since there seems to be a bug in RStudio

--- R/zzz.R
+++ R/zzz.R
@@ -27,12 +27,14 @@
 
 
 .onAttach <- function(libname, pkgname) {
-  # Startup message
+#  Startup message
+#  This works, but only once per session since there seems to be a bug in
+#  RStudio. cf. https://github.com/r-lib/devtools/issues/1442
   m <- sprintf("\nWelcome: this is the %s package.\n", pkgname)
   m <- c(m, sprintf("Author(s):\n  %s\n",
-                  utils::packageDescription(pkgname)$Author))
+                    utils::packageDescription(pkgname)$Author))
   m <- c(m, sprintf("Maintainer:\n  %s\n",
-                  utils::packageDescription(pkgname)$Maintainer))
+                    utils::packageDescription(pkgname)$Maintainer))
 
   packageStartupMessage(paste(m, collapse=""))
 }
